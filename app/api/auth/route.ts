c0850bee5d2ee996dac964c48baa19aca7250077
negar <negar.karimnejad@gmail.com>
2024-01-30 18:18:05 +0330

    set authOptions

---
+++ app/api/auth/route.ts
@@ -0,0 +1,52 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
+import nextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../../lib/prismadb";
+
+export const authOptions:AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/sign-in"
+  },
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = nextAuth( authOptions )
+
+export { handler as GET, handler as POST };
+
