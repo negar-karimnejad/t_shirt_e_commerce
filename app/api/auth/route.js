5697c9f504d70445ed6c7833b936d96919392c97
negar <negar.karimnejad@gmail.com>
2024-01-30 17:15:51 +0330

    set Prisma Adapter

---
+++ app/api/auth/route.js
@@ -0,0 +1,10 @@
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
+// import NextAuth from "next-auth";
+
+// const prisma = new PrismaClient();
+
+// export default NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [],
+// });
